085db14771b8294780f0afd90816acceee13dc18
Eduardo Silveiro <e.silveiro@campus.fct.unl.pt>
2024-12-11 10:39:13 +0000

    PVC e PV a funcionar

--- scc2425-main/scc2425-tukano/src/main/java/tukano/impl/storage/FilesystemStorage.java
+++ scc2425-main/scc2425-tukano/src/main/java/tukano/impl/storage/FilesystemStorage.java
@@ -23,7 +23,7 @@ import utils.IO;
 public class FilesystemStorage implements BlobStorage {
 	private final String rootDir;
 	private static final int CHUNK_SIZE = 4096;
-	private static final String DEFAULT_ROOT_DIR = "/mnt/vol";
+	private static final String DEFAULT_ROOT_DIR = System.getenv("BLOB-PATH");
 
 	public FilesystemStorage() {
 		this.rootDir = DEFAULT_ROOT_DIR;
